352ba5cc5377d75939ffedfc449ed4f7ecbf7e99
BlueDream <1004@posteo.net>
2019-06-27 23:20:15 +0200

    Add files via upload

---
+++ include/Engine/Engine_Pizza.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2019
+** Engine_Pizza.hpp
+** File description:
+** Engine_Pizza
+*/
+
+#ifndef ENGINE_PIZZA_HPP_
+    #define ENGINE_PIZZA_HPP_
+
+    // # Class
+
+    class Pizza {
+        public:
+            Pizza();
+            ~Pizza();
+
+            int type;
+            int state;
+    };
+
+#endif /* !ENGINE_PIZZA_HPP_ */
---
+++ include/Shell/Shell_Reader.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2019
+** Shell_Reader.hpp
+** File description:
+** Shell_Reader
+*/
+
+#ifndef SHELL_READER_HPP_
+    #define SHELL_READER_HPP_
+    
+    char *shell_read_line(void);
+
+#endif /* !SHELL_READER_HPP_ */
